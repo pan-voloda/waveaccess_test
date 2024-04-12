c186a5024d9edd5c101c955282159895722afca6
Wladimir <wladimirs2512@gmail.com>
2024-04-12 19:01:14 +0300

    test1 new vers

--- test1/main.c
+++ test1/main.c
@@ -5,10 +5,11 @@ int main() {
 
     printf("Введите четыре числа: ");
     scanf("%u %u %u %u", &arr[0], &arr[1], &arr[2], &arr[3]);
-
-    max = (arr[0] > arr[1]) ? arr[0] : arr[1];
-    max = (max > arr[2]) ? max : arr[2];
-    max = (max > arr[3]) ? max : arr[3];
+    max=arr[0];
+    for(int i=1; i<=3; i++)
+    {
+    max = (max > arr[i]) ? max : arr[i];
+    }
 
     for(int i=0; i<4; i++)
     {
