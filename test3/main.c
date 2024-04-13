5e74d36f6e72e41d87a6d2a8a4d91a33e8b85c31
Wladimir <wladimirs2512@gmail.com>
2024-04-13 19:09:37 +0300

    small fix

--- test3/main.c
+++ test3/main.c
@@ -176,9 +176,9 @@ int main() {
                     bitValue=json_parser(params, "bit");
                     lenValue=json_parser(params, "len");
 
-                    mask = bits1(lenValue+(8-bitValue));
-                    cmd_str = words[wordValue-1] & mask;
-                    cmd_str = cmd_str >> (16-(lenValue+bitValue));
+                    cmd_str = words[wordValue-1] >> (16-(lenValue+bitValue));
+                    mask = bits1(lenValue);
+                    cmd_str = cmd_str & mask;
                     printf("substring: %02x\n", cmd_str);
                 } 
 
